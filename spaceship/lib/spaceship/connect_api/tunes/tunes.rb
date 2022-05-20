e4fc06770a3990ce1affec5bec239cf77f764269
Matt Blessed <matt@askimpulse.com>
2022-05-20 12:31:43 -0400

    deliver: solidify using the create app clip default experience template option

--- spaceship/lib/spaceship/connect_api/tunes/tunes.rb
+++ spaceship/lib/spaceship/connect_api/tunes/tunes.rb
@@ -1277,11 +1277,10 @@ module Spaceship
         end
 
         # https://developer.apple.com/documentation/appstoreconnectapi/create_a_default_app_clip_experience
-        def post_app_clip_default_experience(app_clip_id: nil, app_store_version_id: nil, attributes: {}, template_default_experience_id: nil)
+        def post_app_clip_default_experience(app_clip_id: nil, app_store_version_id: nil, attributes:, template_default_experience_id: nil)
           body = {
             data: {
               type: "appClipDefaultExperiences",
-              attributes: attributes,
               relationships: {
                 appClip: {
                   data: {
@@ -1298,6 +1297,8 @@ module Spaceship
               }
             }
           }
+
+          body.data.attributes = attributes unless attributes.nil?
 
           unless template_default_experience_id.nil?
             body[:data][:relationships][:appClipDefaultExperienceTemplate] = {
